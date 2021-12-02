bc52f93181fe7b5d6b02e4595e78efa9463b406c
Ruan Montelo <ruanmontelo@gmail.com>
2021-12-02 09:43:51 -0300

    Adding problem explanation for the second part of Day 2

--- AdventOfCode2021/AdventOfCode2021/Day 2/DayTwo.cs
+++ AdventOfCode2021/AdventOfCode2021/Day 2/DayTwo.cs
@@ -42,6 +42,17 @@
             return pos.GetPosition();
         }
         
+        /// <summary>
+        /// Based on your calculations, the planned course doesn't seem to make any sense. You find the submarine manual and discover that the process is actually slightly more complicated.
+        /// In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0.
+        /// The commands also mean something entirely different than you first thought:
+        /// "down X" increases your aim by X units.
+        /// "up X" decreases your aim by X units.
+        /// "forward X" does two things:
+        ///  It increases your horizontal position by X units.
+        ///  It increases your depth by your aim multiplied by X.
+        /// Using this new interpretation of the commands, calculate the horizontal position and depth you would have after following the planned course.
+        /// </summary>
         public override int PartTwo()
         {
             Position pos = new();
